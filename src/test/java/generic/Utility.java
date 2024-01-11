48af3053bfc9e6b77b3e4f33d17c5c7a3ec44289
AksharaBhanu <javagalbhanu@gmail.com>
2024-01-11 08:09:32 +0530

    Added Excel support

--- src/test/java/generic/Utility.java
+++ src/test/java/generic/Utility.java
@@ -3,6 +3,9 @@ package generic;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class Utility {
 
 	public static String getProperty(String path,String key)
@@ -17,9 +20,25 @@ public class Utility {
 		}
 		catch (Exception e) 
 		{
-			
+			e.printStackTrace();
 		}
 		
+		return value;
+	}
+	
+	
+	public static String getExcelData(String path,String sheet,int r,int c)
+	{
+		String value="";
+		try
+		{
+			Workbook wb = WorkbookFactory.create(new FileInputStream(path));
+			value=wb.getSheet(sheet).getRow(r).getCell(c).toString();
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 		return value;
 	}
 }
--- src/test/java/script/TestDemo.java
+++ src/test/java/script/TestDemo.java
@@ -5,13 +5,15 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Utility;
 //i want to run only one script (this one) but on grid
 public class TestDemo extends BaseTest
 {
 	@Test
 	public void test1()
 	{
-		Reporter.log("Bhanu Executing test1....",true);
-		Assert.fail();
+		String v = Utility.getExcelData("./data/input.xlsx", "Sheet1", 0, 0);
+		Reporter.log("Bhanu Executing test1...."+v,true);
+//		Assert.fail();
 	}
 }
