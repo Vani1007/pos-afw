9a8d56f293bd459c5a6eb7e2e015781352c97b00
AksharaBhanu <javagalbhanu@gmail.com>
2024-01-12 07:42:30 +0530

    Data Drive the script1

--- src/test/java/script/ValidLogin.java
+++ src/test/java/script/ValidLogin.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Utility;
 import page.HomePage;
 import page.LoginPage;
 
@@ -12,13 +13,18 @@ public class ValidLogin extends BaseTest
 	@Test(priority = 1)
 	public void testValidLogin()
 	{
+			String un=Utility.getExcelData(XLPATH, "ValidLogin",1,0);
+			String pw=Utility.getExcelData(XLPATH, "ValidLogin",1,1);
 //			1. enter valid user name
 			LoginPage loginPage=new LoginPage(driver);
-			loginPage.setUserName("admin");
+			loginPage.setUserName(un);
+			
 //			2. enter valid password
-			loginPage.setPassword("pointofsale");
+			loginPage.setPassword(pw);
+			
 //			3. click go button
 			loginPage.clicGoButton();
+			
 //			4. verify that home page is displayed
 			HomePage homePage=new HomePage(driver);
 			boolean result = homePage.verifyHomePageIsDisplayed(wait);
